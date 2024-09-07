f33a12560dceca91b2a84f98ba63800f1bdd47ea
magdevux <magsontav@gmail.com>
2024-09-07 16:55:25 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal", 
+        descricao: "Rayssa Leal é uma skatista brasileira, conhecida como a 'Fadinha'. Com apenas [idade], ela já conquistou diversos títulos e se tornou um dos maiores nomes do skate mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de bronze no Mundial de Judô de 2023. Sua técnica e determinação a tornaram uma das principais atletas do judô feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza_(judoca)",
+        tags: "judô judo judoca judoka" 
+      },
+      {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou a medalha de prata nas Olimpíadas de Tóquio 2020. Sua versatilidade e carisma a tornaram uma das atletas mais populares do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+      }
+
+    ];
